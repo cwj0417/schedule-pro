5ff17856aba401a035911c0ea9de20d6be544742
chenwj <731028571@qq.com>
2021-08-01 00:38:30 +0800

    chore: update ci

--- scripts/run-build.js
+++ scripts/run-build.js
@@ -28,7 +28,7 @@ const runBuild = async () => {
     builder.build({
         targets: Platform.MAC.createTarget(),
         config: require(join(projRoot, 'build', 'config.js')),
-        // publish: 'always',
+        publish: 'never',
         // dir: true,
     })
         .then(() => {
